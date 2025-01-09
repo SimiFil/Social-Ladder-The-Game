54463a72eb57c1ae05e692bec0a376173477b27b
Filip Simandl <filip.simandl@gmail.com>
2025-01-09 21:56:16 +0100

    modify game class, rm player struct

--- TheSocialLadderGame/GameManager.swift
+++ TheSocialLadderGame/GameManager.swift
@@ -8,14 +8,6 @@
 import Foundation
 import GameKit
 
-// MARK: Question Types
-enum QuestionsType: String {
-    case basicQuestions = "basicQuestions"
-    case partyQuestions = "partyQuestions"
-    case wildQuestions = "wildQuestions"
-    case customQuestions = "customQuestions"
-}
-
 // MARK: Game State
 enum GameState: String {
     case waitingForPlayers
@@ -26,27 +18,33 @@ enum GameState: String {
 }
 
 // MARK: Game Class
-class Game: ObservableObject {
+class GameManager: ObservableObject {
     // game params
     @Published var roundTime: Int = 120 // 2 minutes to play
     @Published var talkTime: Int = 300 // 5 minutes to talk about it
-    @Published var players: [Player] = []
+    @Published var players: [GameKit.GKLocalPlayer] = []
     @Published var questions: [String] = []
     @Published var currentQuestion: String?
     @Published var gameState: GameState = .waitingForPlayers
     @Published var playerAuthState: PlayerAuthState = .unatuhenticated
     @Published var isHost: Bool = false
+    @Published var isTimeKeeper: Bool = false // so that the timer runs only on one device
+    @Published var isTheChosenOne: Bool = false // one player will be chosen each round to be the "leader" of the round
     @Published var errorMessage: String?
     
     @Published var currentRound: Int = 0
-    let maxRounds: Int = 10
-    let maxPlayers: Int = 8
+    let maxRounds: Int = 10 // MARK: MAX_ROUNDS
+    let minPlayers: Int = 4 // MARK: MIN_PLAYERS
+    let maxPlayers: Int = 8 // MARK: MAX_PLAYERS
 
     private var match: GKMatch?
     private var gameTimer: Timer?
     private let localPlayer = GKLocalPlayer.local // the player on the current device 
 
     // start game func
+    func startGame() -> Void {
+        // TODO
+    }
     
     // load questions func
     func loadQuestions(from type: QuestionsType) -> Bool {
@@ -110,5 +108,10 @@ class Game: ObservableObject {
     
     private struct Question: Codable {
         let question: String
+    }
+    
+    // MARK: Picking player whose order will be important for that round
+    func pickPlayerForRound(from players:[GameKit.GKMatchedPlayers]) {
+        // TODO
     }
 }
---
+++ TheSocialLadderGame/QuestionType.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionType.swift
+//  TheSocialLadderGame
+//
+//  Created by Filip Simandl on 09.01.2025.
+//
+
+import Foundation
+
+// MARK: Question Types
+enum QuestionsType: String {
+    case basicQuestions = "basicQuestions"
+    case partyQuestions = "partyQuestions"
+    case wildQuestions = "wildQuestions"
+    case customQuestions = "customQuestions"
+}
