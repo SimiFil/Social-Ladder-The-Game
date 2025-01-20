54bb01096ec9d22b1418f4646c87b451dd6795c4
Filip Simandl <filip.simandl@gmail.com>
2025-01-20 16:05:36 +0100

    finish HowToPlayView

--- TheSocialLadderGame/Views/HowToPlayView.swift
+++ TheSocialLadderGame/Views/HowToPlayView.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct HowToPlayView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // FIXME: replace colors with images desplaying how to play the game
+        @State var colors: [Color] = [.yellow, .orange, .red, .cyan]
+        
+        GeometryReader { geo in
+            ZStack {
+                AppBackground()
+                
+                // MARK: How to play slider
+                VStack(alignment: .leading, spacing: 1) {
+                    HStack(alignment: .center) {
+                        Text("How To Play")
+                            .font(.title)
+                            .foregroundStyle(.white)
+                            .fontWeight(.bold)
+                     
+                        Image(systemName: "gamecontroller.fill")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .imageScale(.medium)
+                            .rotationEffect(Angle(degrees: 45))
+                    }
+                    .padding(.leading, geo.size.width/15)
+                    .padding(.top, geo.size.width/25)
+                    
+                    TabView {
+                        ForEach(colors, id: \.self) { color in
+                            Text("hi")
+                                .frame(minWidth: geo.size.width/1.15, minHeight: geo.size.height/1.5)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .fill(color)
+                                }
+                        }
+                        
+                    }
+                    .tabViewStyle(.page)
+                    .frame(width: geo.size.width, height: geo.size.height/1.1)
+                    .clipShape(.rect(cornerRadius: 25))
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
