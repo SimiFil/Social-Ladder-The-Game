14de3fa6d634740f1215a87b46cc8cc7904058ca
Filip Simandl <filip.simandl@gmail.com>
2025-02-01 16:01:32 +0100

    fix game loop

--- TheSocialLadderGame/GameManager+GKMatchDelegate.swift
+++ TheSocialLadderGame/GameManager+GKMatchDelegate.swift
@@ -75,8 +75,10 @@ extension GameManager: GKMatchDelegate {
                     }
                 case .roundState:
                     self.roundState = gameData.data["roundPlaying"] != nil ? .playing : .roundEnd
-                    self.currentRound += 1
-                    print(self.currentRound)
+                    if roundState == .roundEnd {
+                        self.currentRound += 1
+                        print(self.currentRound)
+                    }
                 case .playerJoined, .playerLeft:
                     print("do nothing for now...")
                 case .timerSync:
