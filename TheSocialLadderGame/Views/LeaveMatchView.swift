09325f3ede2831db741fe20743ea1d5652983b53
Filip Simandl <filip.simandl@gmail.com>
2025-02-03 11:48:03 +0100

    fix settings layout

---
+++ TheSocialLadderGame/Views/LeaveMatchView.swift
@@ -0,0 +1,18 @@
+//
+//  LeaveMatchView.swift
+//  TheSocialLadderGame
+//
+//  Created by Filip Simandl on 02.02.2025.
+//
+
+import SwiftUI
+
+struct LeaveMatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeaveMatchView()
+}
